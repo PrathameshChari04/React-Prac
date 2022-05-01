b7f80b5cac9d8d633e1aa863a4ea84c2d2864961
PrathameshChari04 <pc939791@gmail.com>
2022-05-01 13:22:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hello from './Hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return(
+        <div>
+        <h1 style={{textAlign:"center"}}>Prathamesh</h1>
+        <Hello name="PPM" />
+        <Hello name="Adword" />
+        </div>
+    )
+};
 
 export default App;
